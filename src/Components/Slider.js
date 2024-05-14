cc2fb7722d968b867c6d5b22b0f329d6454b1eb6
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-05-14 22:44:38 +0500

    Barber-Website-React

---
+++ src/Components/Slider.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Slider() {
+    return (
+        <div id="slider">
+            <div id="slidertext">
+                <h3>Feel like home</h3>
+                <br />
+                <h1>GOOD LOOK GUARANTEED</h1><br />
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, sint. Illum reprehenderit eius fugit et est vel, voluptatem facere illo. Voluptas nihil porro optio animi totam eaque deleniti dolores quasi.</p><br />
+                <button className="btn">Make an appointment</button>&nbsp;&nbsp;
+                <button className="btn">Contact Us</button>
+            </div>
+
+            <div id="sliderimg"></div>
+        </div>
+    )
+}
