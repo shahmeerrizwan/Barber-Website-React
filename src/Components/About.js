50891fbc3260f584613eeccafaade12b8fd5e362
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-05-14 22:46:38 +0500

    Barber-Website-React

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div id="about">
+            <div id="aboutImg"></div>
+            <div id="aboutText">
+                <h4 id="aboutHeading">ABOUT US</h4><br />
+                <h1>Connect With Your Dream Style</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea delectus eius laudantium. Dignissimos odit sit doloremque maiores itaque quibusdam natus vitae neque. Vel laudantium, ipsum nulla adipisci molestias ad ipsa.</p>
+                <br />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate magnam assumenda ex repellendus doloremque ad vero perspiciatis ipsam officiis ea, quasi facere rem ipsa. Ut harum dicta amet iure repellat.</p>
+            </div>
+        </div>
+    )
+}
